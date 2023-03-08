39aa6fe5583d99378f06db4202bf25056da9f1b1
gaston <calderagaston@gmail.com>
2023-03-07 23:03:49 -0400

    change api url base

--- take-home/src/api/index.ts
+++ take-home/src/api/index.ts
@@ -4,7 +4,7 @@ export async function getAllCommits(url: string, page: number) {
   try {
     const baseUrl = process.env.API;
     const arrayUrl = url.split('/').filter((element) => element !== '');
-    const response = await axios.post(`${baseUrl}/commit`, {
+    const response = await axios.post(`${baseUrl}commit`, {
       owner: arrayUrl[arrayUrl.length - 2],
       repo: arrayUrl[arrayUrl.length - 1],
       skip: page,
