c057781110671f5cced9fc3413809f86a635b3b0
gaston <calderagaston@gmail.com>
2023-03-08 21:41:41 -0400

    change cors rule in backend

--- take-home-server/src/main.ts
+++ take-home-server/src/main.ts
@@ -5,7 +5,11 @@ import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- take-home/src/api/index.ts
+++ take-home/src/api/index.ts
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function getAllCommits(url: string, page: number) {
   try {
+    axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
     const baseUrl = process.env.API;
-    const arrayUrl = url.split('/').filter((element) => element !== '');
+    const arrayUrl = url.split("/").filter((element) => element !== "");
     const response = await axios.post(`${baseUrl}commit`, {
       owner: arrayUrl[arrayUrl.length - 2],
       repo: arrayUrl[arrayUrl.length - 1],
